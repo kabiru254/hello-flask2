af0dc7db859326c3b1f5f58cb503647a620a80f9
kabiru254 <xyrisyt@gmail.com>
2023-05-02 17:52:12 +0300

    Working with templates

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    """Index URL"""
+    return render_template('index.html', title='Index Page')
+
+@app.route('/about-me')
+def about_me():
+    """About Me URL"""
+    return render_template('about_me.html', title= 'About Me Page')
